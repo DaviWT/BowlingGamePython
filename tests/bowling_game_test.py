fd89b1b27c4bb16085cf6175977ddfba3c25c517
TokikawD <davi.tokikawa@landisgyr.com>
2023-07-17 18:32:04 -0300

    Add tests to ensure sequential spare and strike bonus scoring support

--- tests/bowling_game_test.py
+++ tests/bowling_game_test.py
@@ -50,3 +50,36 @@ def test_single_strike(game_instance: Game) -> None:
 
     # Assert
     assert game_score == 32
+
+def test_sequential_spare(game_instance: Game) -> None:
+    # Act
+    game_instance.roll(2)
+    game_instance.roll(4)
+    game_instance.roll(5)
+    game_instance.roll(5)
+    game_instance.roll(5)
+    game_instance.roll(5)
+    game_instance.roll(3)
+
+    for _ in range(13):
+        game_instance.roll(0)
+    game_score = game_instance.score()
+
+    # Assert
+    assert game_score == 37
+
+def test_sequential_strike(game_instance: Game) -> None:
+    # Act
+    game_instance.roll(2)
+    game_instance.roll(4)
+    game_instance.roll(10)
+    game_instance.roll(10)
+    game_instance.roll(5)
+    game_instance.roll(3)
+
+    for _ in range(12):
+        game_instance.roll(0)
+    game_score = game_instance.score()
+
+    # Assert
+    assert game_score == 52
