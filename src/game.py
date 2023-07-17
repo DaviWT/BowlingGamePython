4490ba32551a7f490eb702f69a0f94de487f2b31
TokikawD <davi.tokikawa@landisgyr.com>
2023-07-17 17:16:25 -0300

    Implement test to support spare scoring bonus

--- src/game.py
+++ src/game.py
@@ -1,11 +1,24 @@
+from typing import List
+
+from src.roll import Roll
+
 
 class Game:
 
     def __init__(self):
-        self._score = 0
+        self._roll_list: List[Roll] = []
 
     def roll(self, pins: int) -> None:
-        self._score += pins
+        self._roll_list.append(Roll(pins))
 
     def score(self) -> int:
-        return self._score
+        final_score = 0
+        for roll_index in range(0, len(self._roll_list), 2):
+            first_roll = self._roll_list[roll_index].pins
+            second_roll = self._roll_list[roll_index + 1].pins
+            frame_score = first_roll + second_roll
+            final_score += frame_score
+            if first_roll + second_roll == 10:
+                final_score += self._roll_list[roll_index + 2].pins
+
+        return final_score
--- tests/bowling_game_test.py
+++ tests/bowling_game_test.py
@@ -1,7 +1,7 @@
 from src.game import Game
 
 
-def test_worst_game(game_instance: Game) -> None:
+def test_worst_scenario_game(game_instance: Game) -> None:
     # Act
     for _ in range(20):
         game_instance.roll(0)
@@ -19,3 +19,19 @@ def test_all_ones_game(game_instance: Game) -> None:
 
     # Assert
     assert game_score == 20
+
+
+def test_single_spare(game_instance: Game) -> None:
+    # Act
+    game_instance.roll(2)
+    game_instance.roll(4)
+    game_instance.roll(5)
+    game_instance.roll(5)
+    game_instance.roll(3)
+
+    for _ in range(15):
+        game_instance.roll(0)
+    game_score = game_instance.score()
+
+    # Assert
+    assert game_score == 22
